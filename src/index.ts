e253e139de28b4ecac2c59e2b83e8ccc29480f23
hofstede-matheus <hofstede.matheus@gmail.com>
2022-08-19 18:18:06 -0300

    better dependency instantiation

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,11 @@ import { CatsController } from './controllers/CatsController'
 import { GetCatsUseCase } from "./interactors/GetCatsUseCase";
 import { InMemmoryCatsRepository } from "./repositories/InMemmoryCatsRepository";
 
-const catsController = new CatsController(new GetCatsUseCase(new InMemmoryCatsRepository()))
+const inMemmoryCatsRepository = new InMemmoryCatsRepository()
+const getCatsUseCase = new GetCatsUseCase(inMemmoryCatsRepository)
+const catsController = new CatsController(getCatsUseCase)
+
+
 
 async function start() {
   const cats = await catsController.getCats()
@@ -11,4 +15,3 @@ async function start() {
 }
 
 start()
-
